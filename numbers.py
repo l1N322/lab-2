1a875502e644436e32ad1868393518f32dbdd4cd
Danil <pro100.dandi@yandex.ru>
2025-10-01 19:28:54 +0300

    Выполненные задания

---
+++ numbers.py
@@ -0,0 +1,10 @@
+print("Введите два числа: ")
+x = int(input())
+y = int(input())
+print("Введите ещё два числа: ")
+z = int(input())
+w = int(input())
+a = x + y
+b = z + w
+rez = a/b
+print("Результат деления первой суммы на вторую = %.2f" % (rez))
